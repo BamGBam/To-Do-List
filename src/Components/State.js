18bf95e420daff952cb4d705e07a5ed63c451aa7
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-11-21 17:56:00 -0800

    Grouping related states inside of an object

--- src/Components/State.js
+++ src/Components/State.js
@@ -5,7 +5,8 @@ import { useState } from "react";
 const StateFunction = () =>{
     const [person, SetPerson] = useState({
         firstname : '',
-        lastName: ''
+        lastName: '',
+        middleName: ''
     });
 
 }
